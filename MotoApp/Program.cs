a78699ac0d1291b003b9f30b5bdd747fd89c76a3
MikyMike76 <michalgolab01@gmail.com>
2024-10-25 13:42:11 +0200

    25.10

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -4,24 +4,33 @@ using MotoApp.Entities;
 using MotoApp.Data;
 
 var employeeRepository = new SqlRepository<Employee>(new MotoAppDbContext());
-AddEmployees(employeeRepository);
 AddManagers(employeeRepository);
 WriteAllToConsole(employeeRepository);
 
-static void AddEmployees (IRepository<Employee> employeeRepository)
+static void AddBatch<T>(IRepository<T> repository, T[] items)
+    where T : class, IEntity
 {
-    employeeRepository.Add(new Employee { FirstName = "Adam" });
-    employeeRepository.Add(new Employee { FirstName = "Piotr" });
-    employeeRepository.Add(new Employee { FirstName = "Zuzia" });
-    employeeRepository.Save();
+    foreach (var item in items)
+    {
+        repository.Add(item);
+    }
+    repository.Save();
 }
-static void AddManagers (IWriteRepository<Manager> menagerRepository)
+var employees = new[]
+{
+    new Employee { FirstName = "Adam" },
+    new Employee { FirstName = "Adam" },
+    new Employee { FirstName = "Adam" },
+
+};
+AddBatch<Employee>(employeeRepository, employees);
+static void AddManagers(IWriteRepository<Manager> menagerRepository)
 {
     menagerRepository.Add(new Manager { FirstName = "Przemek" });
     menagerRepository.Add(new Manager { FirstName = "Tomek" });
     menagerRepository.Save();
 }
-static void WriteAllToConsole (IReadRepository<IEntity> repository)
+static void WriteAllToConsole(IReadRepository<IEntity> repository)
 {
     var items = repository.GetAll();
     foreach (var item in items)
