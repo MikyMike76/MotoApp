ce911476dc73e861ecc1753ed9219cde615af861
MikyMike76 <michalgolab01@gmail.com>
2024-11-04 10:04:15 +0100

    04.11 - 1

---
+++ MotoApp-master/MotoApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using MotoApp;
+using MotoApp.Repositories;
+using MotoApp.Entities;
+using MotoApp.Data;
+using MotoApp.Repositories.Extensions;
+
+var itemAdded = new ItemAdded(EmployeeAdded); 
+var employeeRepository = new SqlRepository<Employee>(new MotoAppDbContext(), itemAdded);
+AddEmployees(employeeRepository);
+var itemAdded1 = new ItemAdded(BusinessPartnerAdded);
+var businessPartnersRepository = new SqlRepository<BusinessPartner>(new MotoAppDbContext(), itemAdded1);
+AddBusinessPartner(businessPartnersRepository);
+
+static void EmployeeAdded(object item)
+{
+    var employee = (Employee)item;
+    Console.WriteLine($"{employee.FirstName} added");
+}
+static void BusinessPartnerAdded(object item)
+{
+    var employee = (BusinessPartner)item;
+Console.WriteLine($"{employee.Name} added");
+}
+
+static void AddEmployees(IRepository<Employee> employeeRepository)
+{
+    var employees = new[]
+{
+    new Employee { FirstName = "Adam" },
+    new Employee { FirstName = "Adrian" },
+    new Employee { FirstName = "Zuzia" },
+
+};
+    employeeRepository.AddBatch(employees);
+}
+static void AddBusinessPartner(IRepository<BusinessPartner> businessPartnersRepository)
+{
+    var businessPartners = new[]
+{
+    new BusinessPartner { Name = "Przemek" },
+    new BusinessPartner { Name = "Tomek" }
+};
+    businessPartnersRepository.AddBatch(businessPartners);
+}
+
+static void WriteAllToConsole(IReadRepository<IEntity> repository)
+{
+    var items = repository.GetAll();
+    foreach (var item in items)
+    {
+        Console.WriteLine(item);
+    }
+}
+WriteAllToConsole(employeeRepository);
+WriteAllToConsole(businessPartnersRepository);
--- MotoApp-master/MotoApp/Repositories/SqlRepository.cs
+++ MotoApp-master/MotoApp/Repositories/SqlRepository.cs
@@ -3,15 +3,18 @@ using MotoApp.Entities;
 
 namespace MotoApp.Repositories
 {
+    public delegate void ItemAdded(object item);
     public class SqlRepository <T> : IRepository<T> where T : class, IEntity, new()
     {
         private readonly DbSet<T> _dbSet;
         private readonly DbContext _dbContext;
+        private readonly ItemAdded _itemAddedCallBack;
 
-        public SqlRepository(DbContext dbContext)
+        public SqlRepository(DbContext dbContext, ItemAdded? itemAddedCallBack = null)
         {
             _dbContext = dbContext;
             _dbSet = dbContext.Set<T>();
+            _itemAddedCallBack = itemAddedCallBack;
         }
         public IEnumerable<T> GetAll()
         {
@@ -24,6 +27,7 @@ namespace MotoApp.Repositories
         public void Add (T item)
         {
             _dbSet.Add(item);
+            _itemAddedCallBack.Invoke(item);
         }
         public void Remove (T item)
         {
